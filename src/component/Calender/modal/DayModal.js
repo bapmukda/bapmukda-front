fd0ed6cbdfd27a03ba0030758ec13891487aabe2
jangjaeko <jangjaeko45@gmail.com>
2021-09-16 19:43:49 +0900

    arrange daymodal

--- src/component/Calender/modal/DayModal.js
+++ src/component/Calender/modal/DayModal.js
@@ -1,33 +1,6 @@
 import React from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {Modalize} from 'react-native-modalize';
-const styles = StyleSheet.create({
-  ContainImoji: {
-    backgroundColor: '#FBFBFB',
-    borderWidth: 1,
-    borderColor: '#EBEBEC',
-    shadowOffset: {width: 2, height: 2},
-    width: 35,
-    height: 35,
-    margin: 20,
-    shadowColor: 'rgb(196, 196, 196)',
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  ContainFull: {
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    shadowOpacity: 1,
-    shadowOffset: {width: 0, height: 1},
-    width: '80%',
-    height: 70,
-    shadowColor: 'rgb(196, 196, 196)',
-    alignItems: 'center',
-    borderRadius: 10,
-    marginBottom: 30,
-  },
-});
 
 export default function DayModal(props) {
   return (
@@ -58,23 +31,10 @@ export default function DayModal(props) {
           {' '}
           {props.isMonth}월 {props.isDay}일 {props.isDate}
         </Text>
-        <View
-          style={{
-            flexDirection: 'row',
-            backgroundColor: '#ffffff',
-            shadowOpacity: 1,
-            shadowOffset: {width: 0, height: 1},
-            width: '80%',
-            height: 70,
-            shadowColor: 'rgb(196, 196, 196)',
-            alignItems: 'center',
-            borderRadius: 10,
-            marginBottom: 30,
-          }}>
+        <View style={styles.ContainFull}>
           <View
             style={{
               backgroundColor: '#FBFBFB',
-
               shadowOffset: {width: 2, height: 2},
               width: 35,
               height: 35,
@@ -121,14 +81,7 @@ export default function DayModal(props) {
             marginBottom: 30,
             marginTop: 30,
           }}>
-          <View
-            style={{
-              width: 8,
-              height: 8,
-              marginTop: 4,
-              borderRadius: 35,
-              backgroundColor: 'green',
-            }}></View>
+          <View style={[styles.dotdot, {backgroundColor: '#7D9E6B'}]}></View>
           <Text style={{marginLeft: 2, fontSize: 13}}> 아침</Text>
           <Text style={{marginLeft: 2}}> 09:00</Text>
 
@@ -139,33 +92,8 @@ export default function DayModal(props) {
           </Text>
         </View>
 
-        <View
-          style={{
-            flexDirection: 'row',
-            backgroundColor: '#ffffff',
-            shadowOpacity: 1,
-            shadowOffset: {width: 0, height: 1},
-            width: '80%',
-            height: 70,
-            shadowColor: 'rgb(196, 196, 196)',
-            alignItems: 'center',
-            borderRadius: 10,
-            marginBottom: 30,
-          }}>
-          <View
-            style={{
-              backgroundColor: '#FBFBFB',
-              borderWidth: 1,
-              borderColor: '#EBEBEC',
-              shadowOffset: {width: 2, height: 2},
-              width: 35,
-              height: 35,
-              margin: 20,
-              shadowColor: 'rgb(196, 196, 196)',
-              borderRadius: 10,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+        <View style={styles.ContainFull}>
+          <View style={styles.ContainImoji}>
             <Text style={{textAlign: 'center', fontSize: 20, width: 20}}>
               🍳
             </Text>
@@ -174,45 +102,13 @@ export default function DayModal(props) {
         </View>
 
         <View style={{flexDirection: 'row', left: '-13%', marginBottom: 30}}>
-          <View
-            style={{
-              width: 8,
-              height: 8,
-              marginTop: 4,
-              borderRadius: 35,
-              backgroundColor: 'red',
-            }}></View>
+          <View style={[styles.dotdot, {backgroundColor: '#E17551'}]}></View>
           <Text style={{marginLeft: 2, fontSize: 13}}> 점심</Text>
           <Text style={{marginLeft: 2}}> 09:00</Text>
-          <Text style={{left: '1200%', color: 'blue'}}> - 30분</Text>
+          <Text style={styles.minusFont}> - 30분</Text>
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            backgroundColor: '#ffffff',
-            shadowOpacity: 1,
-            shadowOffset: {width: 0, height: 1},
-            width: '80%',
-            height: 70,
-            shadowColor: 'rgb(196, 196, 196)',
-            alignItems: 'center',
-            borderRadius: 10,
-            marginBottom: 30,
-          }}>
-          <View
-            style={{
-              backgroundColor: '#FBFBFB',
-              borderWidth: 1,
-              borderColor: '#EBEBEC',
-              shadowOffset: {width: 2, height: 2},
-              width: 35,
-              height: 35,
-              margin: 20,
-              shadowColor: 'rgb(196, 196, 196)',
-              borderRadius: 10,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+        <View style={styles.ContainFull}>
+          <View style={styles.ContainImoji}>
             <Text style={{textAlign: 'center', fontSize: 20, width: 20}}>
               🍳
             </Text>
@@ -221,45 +117,13 @@ export default function DayModal(props) {
         </View>
 
         <View style={{flexDirection: 'row', left: '-13%', marginBottom: 30}}>
-          <View
-            style={{
-              width: 8,
-              height: 8,
-              marginTop: 4,
-              borderRadius: 35,
-              backgroundColor: 'orange',
-            }}></View>
+          <View style={[styles.dotdot, {backgroundColor: '#F7BC6E'}]}></View>
           <Text style={{marginLeft: 2, fontSize: 13}}> 저녁</Text>
           <Text style={{marginLeft: 2}}> 19:00</Text>
-          <Text style={{left: '1200%', color: 'blue'}}> - 00분</Text>
+          <Text style={styles.minusFont}> - 00분</Text>
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            backgroundColor: '#ffffff',
-            shadowOpacity: 1,
-            shadowOffset: {width: 0, height: 1},
-            width: '80%',
-            height: 70,
-            shadowColor: 'rgb(196, 196, 196)',
-            alignItems: 'center',
-            borderRadius: 10,
-            marginBottom: 30,
-          }}>
-          <View
-            style={{
-              backgroundColor: '#FBFBFB',
-              borderWidth: 1,
-              borderColor: '#EBEBEC',
-              shadowOffset: {width: 2, height: 2},
-              width: 35,
-              height: 35,
-              margin: 20,
-              shadowColor: 'rgb(196, 196, 196)',
-              borderRadius: 10,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
+        <View style={styles.ContainFull}>
+          <View style={styles.ContainImoji}>
             <Text style={{textAlign: 'center', fontSize: 20, width: 20}}>
               🍳
             </Text>
@@ -269,17 +133,10 @@ export default function DayModal(props) {
         </View>
 
         <View style={{flexDirection: 'row', left: '-13%', marginBottom: 30}}>
-          <View
-            style={{
-              width: 8,
-              height: 8,
-              marginTop: 4,
-              borderRadius: 35,
-              backgroundColor: 'blue',
-            }}></View>
+          <View style={[styles.dotdot, {backgroundColor: '#738CC1'}]}></View>
           <Text style={{marginLeft: 2, fontSize: 13}}> 간식</Text>
           <Text style={{marginLeft: 2}}> 19:00</Text>
-          <Text style={{left: '1200%', color: 'blue'}}> - 00분</Text>
+          <Text style={styles.minusFont}> - 00분</Text>
         </View>
         <View style={styles.ContainFull}>
           <View style={styles.ContainImoji}>
@@ -293,3 +150,42 @@ export default function DayModal(props) {
     </Modalize>
   );
 }
+
+const styles = StyleSheet.create({
+  ContainImoji: {
+    backgroundColor: '#FBFBFB',
+    borderWidth: 1,
+    borderColor: '#EBEBEC',
+    shadowOffset: {width: 2, height: 2},
+    width: 35,
+    height: 35,
+    margin: 20,
+    shadowColor: 'rgb(196, 196, 196)',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ContainFull: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    shadowOpacity: 1,
+    shadowOffset: {width: 0, height: 1},
+    width: '80%',
+    height: 70,
+    shadowColor: 'rgb(196, 196, 196)',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginBottom: 30,
+  },
+  dotdot: {
+    width: 8,
+    height: 8,
+    marginTop: 4,
+    borderRadius: 35,
+  },
+  minusFont: {
+    left: '1200%',
+    color: '#738CC1',
+    fontWeight: 'bold',
+  },
+});
