2369be6a879c3e9f5139eb2ab5f874285102e7ae
jangjaeko <jangjaeko45@gmail.com>
2021-10-24 20:24:35 +0900

    set swipe to delete

--- src/component/Calender/modal/swipetoDelete.js
+++ src/component/Calender/modal/swipetoDelete.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+} from 'react-native';
+import {Swipeable} from 'react-native-gesture-handler';
+
+const SCREEN_WIDTH = Dimensions.get(window).width;
 
 export default function swipetoDelete() {
+  const [lists, setLists] = [];
+  const leftSwipe = (progress, dragX) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    });
+    return (
+      <TouchableOpacity activeOpacity={0.6} onPress={}>
+        <View style={styles.deleteBox}>
+          <Animated.Text style={{transform: [{scale: scale}]}}>
+            Delete
+          </Animated.Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const deleteItem = index => {
+    const arr = [...lists];
+    arr.splice(index, 1);
+    setLists(arr);
+  };
+
   return (
-    <View>
-      <Text>start swipe to delete</Text>
-    </View>
+    <Swipeable renderLeftActions={leftSwipe}>
+      <View>
+        <Text>start swipe to delete</Text>
+      </View>
+    </Swipeable>
   );
 }
+
+const styles = StyleSheet.create({
+  deleteBox: {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    width: 100,
+  },
+});
